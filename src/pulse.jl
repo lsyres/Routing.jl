91050e2ae02180d6d7239d03ac198d77f01c9fcd
Changhyun Kwon <chkwon@gmail.com>
2020-12-18 15:23:26 -0500

    cleaning up

--- src/pulse.jl
+++ src/pulse.jl
@@ -97,177 +97,145 @@ function should_rollback(p::Pulse, pg::PulseGraph)
     end
 end
 
+function graph_reduction!(pg::PulseGraph)
+    for i in 1:pg.origin-1, j in 1:pg.origin-1
+        if i != j 
+            if pg.early_time[i] + pg.time[i, j] > pg.late_time[j]
+                pg.cost[i, j] = Inf
+            end
+        end
+    end
+end
 
+function time_to_time_value_index(t, time_values)
+    Δ = time_values[1] - time_values[2] 
+    time_ub = time_values[1]
+    k = Int(ceil((time_ub - t) / Δ)) + 1
+    return k
+end
 
+function isbounded(p::Pulse, best_p::Pulse, lower_bounds, time_values)
+    if isempty(lower_bounds) || isempty(time_values)
+        return false
+    end
 
-# function solveESPPRC(origin, destination, resrc_cap, cost_mtx, time_mtx, resrc_mtx, tw_mtx; allow_suboptimal=true, route_sets=[])
+    upper_bound = best_p.cost
+    # must use from the bound matrix B the lower closest value to τ available. 
+    # time_values (sorted from greatest to smallest)
+    # τ <= p.time
+    k = time_to_time_value_index(p.time, time_values)
+
+    if k > length(time_values)
+        return false
+    else
+        bounded = p.cost + lower_bounds[p.next, k] >= upper_bound
+        return bounded
+    end
+end
+
+    
+function bounding_scheme(pg::PulseGraph)
+    time_ub = pg.late_time[pg.destination]
+    time_lb = 0.1 * time_ub
+    Δ = Int(floor((time_ub-time_lb) / 15))
+    # Δ = 10
+    # @show time_ub, time_lb, Δ
+
+    time_values = collect(time_ub:-Δ:time_lb)
+    lower_bounds = fill(-Inf, length(pg.cost[:,1]), length(time_values))
+    best_pulse_labels = Array{Pulse, 1}(undef, length(pg.cost[:,1]))
+    for i in eachindex(best_pulse_labels)
+        best_pulse_labels[i] = initialize_pulse(i; cost=Inf)
+    end
+
+    _pg = PulseGraph(1, pg.destination, pg.capacity, pg.cost, pg.time, pg.load, pg.early_time, pg.late_time)
+    p = initialize_pulse(1)
+    p_star = initialize_pulse(1; cost=Inf)
+
+    bounding_iteration = 0
+
+    for k in 2:length(time_values) 
+        for v_i in 1:pg.destination-1
+            τ = time_values[k]
+
+            initialize_pulse!(p, v_i)
+            p.time = τ 
+            p_star = best_pulse_labels[v_i]
+            _pg.origin = v_i
+
+            pulse_procedure!(v_i, p, p_star, lower_bounds, time_values, _pg)
+            if p_star.path == [] 
+                lower_bounds[v_i, k] = Inf
+            else
+                # Thie line below isn't necessary, as we call by reference.
+                # best_pulse_labels[v_i] = deepcopy(p_star) 
+                lower_bounds[v_i, k] = p_star.cost 
+            end
+            
+            bounding_iteration += 1
+
+        end
+    end
+
+    return lower_bounds, time_values
+end
+
+function pulse_procedure!(v_i::Int, p::Pulse, best_p::Pulse, lower_bounds, time_values, pg::PulseGraph)
+    # v_i = current node
+    @assert v_i == p.next
+
+    if p.time < pg.early_time[v_i]
+        p.time = pg.early_time[v_i]
+    end
+
+    if !isfeasible(p, pg) 
+        return
+    elseif isbounded(p, best_p, lower_bounds, time_values)
+        return
+    elseif should_rollback(p, pg)
+        return
+    end
+
+    # Update the best route found so far
+    if p.next == pg.destination 
+        if p.cost < best_p.cost
+            best_p.path = p.path
+            push!(best_p.path, p.next)
+            best_p.next = -1                
+            best_p.cost = p.cost 
+            best_p.load = p.load
+            best_p.time = p.time
+        end
+        return
+    end
+        
+    # Create a new pulse 
+    forward_star = findall(x -> x < Inf, pg.cost[v_i, :])
+    for v_j in forward_star
+        if v_j != p.next && ! in(v_j, p.path)
+            pp = deepcopy(p)
+            push!(pp.path, pp.next)
+            pp.next = v_j 
+            pp.cost += pg.cost[v_i, v_j]
+            pp.load += pg.load[v_i, v_j]
+            pp.time = max(pg.early_time[v_j], pp.time + pg.time[v_i, v_j]) 
+            pulse_procedure!(v_j, pp, best_p, lower_bounds, time_values, pg)
+        end
+    end
+
+    # 
+end
 
 function solveESPPRCpulse(org_pg::PulseGraph)
-    counter = 0 
-    
-    function graph_reduction!(pg::PulseGraph)
-        for i in 1:pg.origin-1, j in 1:pg.origin-1
-            if i != j 
-                if pg.early_time[i] + pg.time[i, j] > pg.late_time[j]
-                    pg.cost[i, j] = Inf
-                end
-            end
-        end
-    end
-
-    function bounding_scheme(pg::PulseGraph)
-        bounding_counter = 0        
-        time_ub = pg.late_time[pg.destination]
-        time_lb = 0.1 * time_ub
-        Δ = Int(floor((time_ub-time_lb) / 15))
-        # Δ = 10
-        # @show time_ub, time_lb, Δ
-
-        time_values = collect(time_ub:-Δ:time_lb)
-        lower_bounds = fill(-Inf, length(pg.cost[:,1]), length(time_values))
-        best_pulse_labels = Array{Pulse, 1}(undef, length(pg.cost[:,1]))
-        for i in eachindex(best_pulse_labels)
-            best_pulse_labels[i] = initialize_pulse(i; cost=Inf)
-        end
-
-        _pg = PulseGraph(1, pg.destination, pg.capacity, pg.cost, pg.time, pg.load, pg.early_time, pg.late_time)
-        p = initialize_pulse(1)
-        p_star = initialize_pulse(1; cost=Inf)
-
-        bounding_iteration = 0
-
-        for k in 2:length(time_values) 
-            for v_i in 1:pg.destination-1
-                τ = time_values[k]
-
-                initialize_pulse!(p, v_i)
-                p.time = τ 
-                p_star = best_pulse_labels[v_i]
-                _pg.origin = v_i
-
-                pulse_procedure!(v_i, p, p_star, lower_bounds, time_values, _pg)
-                if p_star.path == [] 
-                    lower_bounds[v_i, k] = Inf
-                else
-                    # Thie line below isn't necessary, as we call by reference.
-                    # best_pulse_labels[v_i] = deepcopy(p_star) 
-                    lower_bounds[v_i, k] = p_star.cost 
-                end
-                
-                bounding_iteration += 1
-
-            end
-        end
-
-        return lower_bounds, time_values
-    end
-
-    function time_to_time_value_index(t, time_values)
-        Δ = time_values[1] - time_values[2] 
-        time_ub = time_values[1]
-        k = Int(ceil((time_ub - t) / Δ)) + 1
-        return k
-    end
-
-    function isbounded(p::Pulse, best_p::Pulse, lower_bounds, time_values)
-        if isempty(lower_bounds) || isempty(time_values)
-            return false
-        end
-
-
-        upper_bound = best_p.cost
-        # must use from the bound matrix B the lower closest value to τ available. 
-        # time_values (sorted from greatest to smallest)
-        # τ <= p.time
-
-        # bit_idx = [time_values .<= p.time]
-        # τ = maximum(time_values .* bit_idx[1])
-        k = time_to_time_value_index(p.time, time_values)
-
-        if k > length(time_values)
-            return false
-        else
-            bounded = p.cost + lower_bounds[p.next, k] >= upper_bound
-            return bounded
-        end
-    end
-
-    function pulse_procedure!(v_i::Int, p::Pulse, best_p::Pulse, lower_bounds, time_values, pg::PulseGraph)
-        # v_i = current node
-        @assert v_i == p.next
-
-        bounding_counter += 1
-
-        if p.time < pg.early_time[v_i]
-            p.time = pg.early_time[v_i]
-        end
-
-        if !isfeasible(p, pg) 
-            return
-        elseif isbounded(p, best_p, lower_bounds, time_values)
-            return
-        elseif should_rollback(p, pg)
-            return
-        end
-
-        counter += 1
-
-        # Update the best route found so far
-        if p.next == pg.destination 
-            if p.cost < best_p.cost
-                best_p.path = p.path
-                push!(best_p.path, p.next)
-                best_p.next = -1                
-                best_p.cost = p.cost 
-                best_p.load = p.load
-                best_p.time = p.time
-            end
-            return
-        end
-            
-        # Create a new pulse 
-        forward_star = findall(x -> x < Inf, pg.cost[v_i, :])
-        for v_j in forward_star
-            if v_j != p.next && ! in(v_j, p.path)
-                pp = deepcopy(p)
-                push!(pp.path, pp.next)
-                pp.next = v_j 
-                pp.cost += pg.cost[v_i, v_j]
-                pp.load += pg.load[v_i, v_j]
-                pp.time = max(pg.early_time[v_j], pp.time + pg.time[v_i, v_j]) 
-                bounding_inner_counter += 1
-                pulse_procedure!(v_j, pp, best_p, lower_bounds, time_values, pg)
-            end
-        end
-
-        # 
-    end
-
-
     pg = deepcopy(org_pg)
     graph_reduction!(pg)
 
-    # println("------Bounding scheme start------------------------------------")
-    # bounding_start = time()
-    bounding_counter = 0  
-    bounding_inner_counter = 0  
-    # @time 
     lower_bounds, time_values = bounding_scheme(pg)
-    # println("Bounding scheme took: $(time()-bounding_start) seconds......")
-    # @show bounding_counter, bounding_inner_counter
-    # println("------Bounding scheme end------------------------------------")
 
     p = initialize_pulse(pg.origin)
     best_p = initialize_pulse(pg.origin; cost=Inf)
 
-
-    # println("------pulse_procedure start------------------------------------")
-    # start_pulse = time()
-    # @time 
     pulse_procedure!(pg.origin, p, best_p, lower_bounds, time_values, pg)
-    # println("Pulse Procedure took: $(time()-start_pulse) seconds......")
-    # println("------pulse_procedure end------------------------------------")
-
 
     return best_p
 end
--- src/vrp_bnb.jl
+++ src/vrp_bnb.jl
@@ -114,120 +114,122 @@ function show_current(sol_y, sol_routes)
 end
 
 
-function solve_vrp_bnb(vrptw::VRPTW_Instance; tw_reduce=true)
+function solve_BnB!(best_sol::BestIncumbent, root_branch)
+    branches = [root_branch]
+    next_branches = Branch[]
 
-    function solve_BnB!(best_sol::BestIncumbent, root_branch)
-        branches = [root_branch]
-        next_branches = Branch[]
+    while !isempty(branches)
+        sort!(branches, by= x->x.lower_bound, rev=true)
+        current_branch = pop!(branches)
 
-        while !isempty(branches)
-            branch_counter += 1
-            sort!(branches, by= x->x.lower_bound, rev=true)
-            current_branch = pop!(branches)
+        # current_branch.branching_priority is updated here
+        arc, new_branch_priority = select_new_arc(current_branch.history, current_branch.branch_priority, current_branch.vrptw_instance)
 
-            # current_branch.branching_priority is updated here
-            arc, new_branch_priority = select_new_arc(current_branch.history, current_branch.branch_priority, vrptw)
+        sol_y, sol_routes, sol_obj = [], [], Inf
+        for arc_value in 0:1
 
-            sol_y, sol_routes, sol_obj = [], [], Inf
-            for arc_value in 0:1
+            # branching history is updated here
+            new_history = copy(current_branch.history)
+            new_history[arc] = arc_value
 
-                # branching history is updated here
-                new_history = copy(current_branch.history)
-                new_history[arc] = arc_value
-
-                new_routes, new_vrptw = generate_branch_instance(arc, arc_value, current_branch)
-                
-                if isempty(new_routes)
-                    sol_y, sol_routes, sol_obj = [], [], Inf
-                else
-                    sol_y, sol_routes, sol_obj = solve_cg_rmp(new_vrptw, initial_routes=new_routes, tw_reduce=false)
-                end
-
-                if isempty(sol_y)
-                    # stop. Infeasible
-
-                elseif is_binary(sol_y)
-                    if sol_obj < best_sol.objective
-                        best_sol.y = sol_y
-                        best_sol.routes = sol_routes
-                        best_sol.objective = sol_obj
-                    end
-                    # stop. a binary feasible solution found. 
-                    
-                elseif sol_obj < best_sol.objective
-                    # Fractional LP Solution. Still alive. Move on 
-                    push!(next_branches, Branch(new_history, new_vrptw, 
-                                                current_branch.root_routes, new_branch_priority, sol_obj))
-
-                else 
-                    # stop. Fathomed. 
-
-                end
-            end
-
-            if isempty(branches)
-                branches = deepcopy(next_branches)
-                next_branches = Branch[]
-            end
+            new_routes, new_vrptw = generate_branch_instance(arc, arc_value, current_branch)
             
+            if isempty(new_routes)
+                sol_y, sol_routes, sol_obj = [], [], Inf
+            else
+                sol_y, sol_routes, sol_obj = solve_cg_rmp(new_vrptw, initial_routes=new_routes, tw_reduce=false)
+            end
+
+            if isempty(sol_y)
+                # stop. Infeasible
+
+            elseif is_binary(sol_y)
+                if sol_obj < best_sol.objective
+                    best_sol.y = sol_y
+                    best_sol.routes = sol_routes
+                    best_sol.objective = sol_obj
+                end
+                # stop. a binary feasible solution found. 
+                
+            elseif sol_obj < best_sol.objective
+                # Fractional LP Solution. Still alive. Move on 
+                push!(next_branches, Branch(new_history, new_vrptw, 
+                                            current_branch.root_routes, new_branch_priority, sol_obj))
+
+            else 
+                # stop. Fathomed. 
+
+            end
         end
 
+        if isempty(branches)
+            branches = deepcopy(next_branches)
+            next_branches = Branch[]
+        end
+        
     end
 
+end
 
-    function generate_branch_priority(root_y, root_routes)
-        branch_score_dict = Dict()
-        for n in eachindex(root_y)
-            if root_y[n] > 0.01
-                r = root_routes[n]
-                for i in 1:length(r)-1
-                    arc = (r[i], r[i+1])
-                    if in(arc, keys(branch_score_dict))
-                        branch_score_dict[arc] += root_y[n]
-                    else
-                        branch_score_dict[arc] = root_y[n]
-                    end
+
+function generate_branch_priority(root_y, root_routes)
+    branch_score_dict = Dict()
+    for n in eachindex(root_y)
+        if root_y[n] > 0.01
+            r = root_routes[n]
+            for i in 1:length(r)-1
+                arc = (r[i], r[i+1])
+                if in(arc, keys(branch_score_dict))
+                    branch_score_dict[arc] += root_y[n]
+                else
+                    branch_score_dict[arc] = root_y[n]
                 end
             end
-        end        
-        return sort(collect(branch_score_dict), by= x->x[2])
-    end
+        end
+    end        
+    return sort(collect(branch_score_dict), by= x->x[2])
+end
 
-    function initial_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
-        branch_priority = generate_branch_priority(root_y, root_routes)
-        new_vrptw = deepcopy(vrptw)
-        history = OrderedDict()
+function initial_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
+    branch_priority = generate_branch_priority(root_y, root_routes)
+    new_vrptw = deepcopy(vrptw)
+    history = OrderedDict()
 
-        for n in eachindex(root_y)
-            if root_y[n] > 1.0 - 1e-6
-                r = root_routes[n]
-                for i in 1:length(r)-1
-                    arc = (r[i], r[i+1])
-                    history[arc] = 1
-                    update_travel_time!(new_vrptw, arc, 1)
-                end
+    for n in eachindex(root_y)
+        if root_y[n] > 1.0 - 1e-6
+            r = root_routes[n]
+            for i in 1:length(r)-1
+                arc = (r[i], r[i+1])
+                history[arc] = 1
+                update_travel_time!(new_vrptw, arc, 1)
             end
-        end                
+        end
+    end                
 
-        initial_branch = Branch(history, new_vrptw, root_routes, branch_priority, root_obj)
+    initial_branch = Branch(history, new_vrptw, root_routes, branch_priority, root_obj)
 
-        solve_BnB!(best_sol, initial_branch)        
-    end
+    solve_BnB!(best_sol, initial_branch)        
+end
 
-    function complete_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
-        # Most Priority is at the end.
-        branch_priority = generate_branch_priority(root_y, root_routes)
-        history = OrderedDict()
+function complete_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
+    # Most Priority is at the end.
+    branch_priority = generate_branch_priority(root_y, root_routes)
+    history = OrderedDict()
 
-        root_branch = Branch(history, vrptw, root_routes, branch_priority, root_obj)
+    root_branch = Branch(history, vrptw, root_routes, branch_priority, root_obj)
 
-        solve_BnB!(best_sol, root_branch)
-    end
+    solve_BnB!(best_sol, root_branch)
+end
 
+function solve_vrp_bnb(vrptw::VRPTW_Instance; tw_reduce=true)
+
+
+    start_time = time()
 
     # Time Windows Reduction 
     if tw_reduce
         time_window_reduction!(vrptw)
+        println("Time Windows Reduced. Cumulative Time: ", time() - start_time)
     end
 
     # initialization
@@ -236,17 +238,13 @@ function solve_vrp_bnb(vrptw::VRPTW_Instance; tw_reduce=true)
     # solve root node 
     @info("Solving the root LP relaxation...")
     root_y, root_routes, root_obj = solve_cg_rmp(vrptw, initial_routes=[], tw_reduce=false)
+    println("Root node solved. Cumulative Time: ", time() - start_time)
     @show length(root_routes)
 
     @info("Root LP relaxation solution:")
     @show root_obj
     show_current(root_y, root_routes)
     println("Is the root solution binary? ", is_binary(root_y))
-
-    branch_counter = 0
-
-    # readline()
-
 
     if is_binary(root_y)
         best_sol.y = root_y 
@@ -257,15 +255,15 @@ function solve_vrp_bnb(vrptw::VRPTW_Instance; tw_reduce=true)
         initial_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
 
         @info("Initial BnB is done.")
-        @show branch_counter
+        println("Initial BnB. Cumulative Time: ", time() - start_time)
         @show best_sol.objective
         show_current(best_sol.y, best_sol.routes)
 
-        branch_counter = 0
         complete_BnB!(best_sol, vrptw, root_y, root_routes, root_obj)
 
         @info("Complete BnB is done.")
-        @show branch_counter
+        println("Complete BnB. Cumulative Time: ", time() - start_time)
+
         @show best_sol.objective
         show_current(best_sol.y, best_sol.routes)
 
