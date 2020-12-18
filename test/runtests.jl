2ce36e20407fa3482bba34312a1c626c9a17f667
Changhyun Kwon <chkwon@gmail.com>
2020-12-18 16:27:21 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using VRPTW
 using Test
 
+include("solve_solomon_vrptw.jl")
+
+
 @testset "VRPTW.jl" begin
     # Write your tests here.
+    solomon = generate_solomon_vrptw_instance("R102_025")
+    @time sol_y, sol_routes, sol_obj = solve_vrp_bnb(solomon.vrptw, tw_reduce=false);
+    @test isapprox(sol_obj, 548.1078177906317, atol=1e-7)
+
 end
