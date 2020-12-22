44a3b46774ed2ed49382e2aeed27c2088227f4d1
Changhyun Kwon <chkwon@gmail.com>
2020-12-22 13:59:08 -0500

    feillet first try

---
+++ feillet/feillet_test.jl
@@ -0,0 +1,149 @@
+# Solving ESPPRC 
+# The example given by Google OR-Tools https://developers.google.com/optimization/routing/vrp
+# Modified
+
+using VRPTW
+
+include("feillet.jl")
+
+using Test 
+
+# For testing purpose
+using Random
+Random.seed!(123432)
+
+
+# We need to construct a PulseGraph instance
+# mutable struct PulseGraph
+#     origin      :: Int64
+#     destination :: Int64
+#     capacity    :: Float64
+#     cost        :: Matrix{Float64}
+#     time        :: Matrix{Float64}
+#     load        :: Matrix{Float64}
+#     early_time  :: Vector{Float64}
+#     late_time   :: Vector{Float64}
+# end
+
+# This example is modifed from the VRPTW example from OR-Tools
+# But ESPPRC can be used outside the VRP context
+# This example describes non-VRP context
+
+# Number of nodes
+n_nodes = 17
+
+# cost matrix:
+# The objective of ESPPRC is to minimize the total cost 
+# Randomly generated in this example 
+# cost allows negative values
+cost = rand(17, 17) .- 0.5
+
+# travel_time matrix:
+# Some elements are Inf, meaning disconnected
+# travel_time is used to check the time windows constraints
+# If you have service time in each node, then it should be added to travel_time
+# e.g., travel_time[i,j] = travel_time[i,j] + service_time[i]
+travel_time = Float64[
+    0 8 3 2 6 8 4 8 8 13 7 5 8 12 10 Inf 6; 
+    8 0 11 10 6 3 9 5 8 4 15 14 13 9 18 9 9; 
+    3 11 0 1 7 10 6 10 10 14 6 7 9 14 6 16 8; 
+    2 10 1 0 6 9 4 8 9 13 4 6 8 12 8 14 7; 
+    6 6 7 6 0 2 3 2 2 7 9 7 7 6 12 8 3; 
+    8 3 10 9 2 0 6 2 5 4 Inf 10 10 6 Inf 5 6; 
+    4 9 6 4 3 6 0 4 4 8 5 4 3 7 8 10 2; 
+    8 5 10 8 2 2 4 0 3 4 9 8 7 3 13 6 3; 
+    8 8 10 9 2 5 4 3 0 4 6 5 4 3 9 5 2; 
+    13 4 14 13 7 4 8 4 4 0 10 9 8 4 13 4 6; 
+    7 Inf 6 4 9 Inf 5 9 6 10 0 1 3 7 3 10 6; 
+    5 14 7 6 7 10 4 8 5 9 1 0 2 6 4 8 4; 
+    8 13 9 8 7 10 3 7 4 8 3 2 0 4 5 6 4; 
+    12 9 14 12 6 6 7 3 3 4 7 6 4 0 9 2 5; 
+    10 18 6 8 12 15 8 13 9 13 3 4 5 9 0 9 9; 
+    Inf 9 16 14 8 5 10 6 5 4 10 8 6 2 9 0 7; 
+    6 9 8 7 3 6 2 3 2 6 6 4 4 5 9 7 0
+]
+
+# load matrix 
+# The load information is for each arc, hence given as a matrix
+# If you have load on each node, then it should be converted to a matrix
+# For example, load[i, j] = load_on_node[j] 
+load_on_node = [1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8, 0]
+load = zeros(n_nodes, n_nodes)
+for i in 1:n_nodes, j in 1:n_nodes
+    load[i, j] = load_on_node[j]
+end
+
+# Time windows, for all nodes
+time_windows = [
+    (0, 12),  # 1
+    (0, 15),  # 2
+    (16, 18),  # 3
+    (10, 13),  # 4
+    (0, 5),  # 5
+    (5, 10),  # 6
+    (0, 4),  # 7
+    (5, 10),  # 8
+    (0, 3),  # 9
+    (10, 16),  # 10
+    (10, 15),  # 11
+    (0, 5),  # 12
+    (5, 10),  # 13
+    (7, 8),  # 14
+    (10, 15),  # 15
+    (11, 15),  # 16
+    (0, 35), # 17
+]
+early_time = [time_windows[i][1] for i in 1:n_nodes]
+late_time = [time_windows[i][2] for i in 1:n_nodes]
+
+# vehicle capacity
+capacity = 15 
+
+# origin and destination
+origin, destination = 1, 17
+
+
+# create a PulseGraph instance
+pg = PulseGraph(
+    origin,
+    destination,
+    capacity,
+    cost,
+    travel_time,
+    load,
+    early_time,
+    late_time
+)
+
+ei = ESPPRC_Instance(
+    origin,
+    destination,
+    capacity,
+    cost,
+    travel_time,
+    load,
+    early_time,
+    late_time,
+    zeros(size(early_time))
+)
+
+# solve the ESPPRC 
+# typeof(sol) == Pulse
+# @time sol = solveESPPRCpulse(pg)
+
+
+@time sol = solveESPPRCpulse(pg)
+@time best_label, labelset = solveESPPRCfeillet(ei)
+
+println("done")
+
+println("Route: ", sol.path)
+println("Total Cost = ", sol.cost)
+println("Total Load = ", sol.load)
+println("Total Time = ", sol.time)
+
+
+@testset "ESPPRC-Example" begin
+    @test isapprox(sol.cost, -1.124519917849354)
+    @test isapprox(best_label.cost, -1.124519917849354)
+end
---
+++ feillet/feillet_test2.jl
@@ -0,0 +1,180 @@
+# Solving ESPPRC 
+# The example given by Google OR-Tools https://developers.google.com/optimization/routing/vrp
+# Modified
+
+using VRPTW
+
+include("feillet.jl")
+
+using Test 
+
+# For testing purpose
+using Random
+Random.seed!(123432)
+using ElasticArrays
+
+dataset_name = "C202_050"
+# dataset_name = "C102_025"
+
+data_file_path = dataset_name * ".xml"
+data_file_path = joinpath(@__DIR__, "..", "solomon-1987", data_file_path)
+dataset, data_name, nodes, fleet, requests = read_solomon_data(data_file_path)
+
+n_nodes = length(nodes)
+n_vehicles = fleet.number
+n_requests = length(requests)
+
+# Add a dummy node for depot at the end
+push!(nodes, nodes[n_nodes])
+depot0 = n_requests + 1
+depot_dummy = n_requests + 2
+
+
+n_nodes = length(nodes)
+@assert n_nodes - 2 == n_requests
+V = 1:n_vehicles
+N = 1:n_nodes
+C = 1:n_requests
+
+d = [requests[i].quantity for i in C]
+q = fleet.capacity
+a = [requests[i].start_time for i in C]
+b = [requests[i].end_time for i in C]
+θ = [requests[i].service_time for i in C]
+
+t = calculate_solomon_cost(nodes)
+t[depot0, depot_dummy] = Inf
+t[depot_dummy, depot0] = Inf
+time_mtx = copy(t)
+
+origin = depot0
+destination = depot_dummy
+capacity = fleet.capacity
+
+alpha = rand(0:20, n_nodes) * 2
+cost_mtx = t - repeat(alpha, 1, n_nodes)
+cost_mtx = t .* (rand(size(t)...) .- 0.10)
+
+
+for i in N, j in N 
+    if i in C
+        time_mtx[i, j] = time_mtx[i, j] + requests[i].service_time
+    else 
+        time_mtx[i, j] = time_mtx[i, j]
+    end
+end
+
+resrc_mtx = zeros(n_nodes, n_nodes)
+for i in N, j in N 
+    if j in C
+        resrc_mtx[i, j] = requests[j].quantity
+    end
+end
+early_time = [a; 0; 0]
+late_time = [b; 0; fleet.max_travel_time]
+service_time = [θ; 0; 0]
+travel_time = copy(t)
+
+
+ei = ESPPRC_Instance(
+    origin,
+    destination,
+    capacity,
+    cost_mtx,
+    travel_time,
+    resrc_mtx,
+    early_time,
+    late_time,
+    service_time
+)
+
+
+
+# pulse_path = solveESPPRC(origin, destination, resrc_cap, cost_mtx, time_mtx, resrc_mtx, early_time, late_time)
+# @show pulse_path.path, pulse_path.cost, pulse_path.time
+
+pg = PulseGraph(
+    origin,
+    destination,
+    capacity,
+    cost_mtx,
+    time_mtx,
+    resrc_mtx,
+    early_time,
+    late_time,
+)
+
+
+
+##############################################################################################################
+
+
+@time sol = solveESPPRCpulse(pg)
+@time lab, labelset = solveESPPRCfeillet(ei)
+
+@show sol.cost, sol.load, sol.time
+@show lab.cost, lab.load, lab.time
+@show sol.path
+@show lab.path
+@assert isapprox(sol.cost, lab.cost, atol=1e-7)
+
+println("done")
+
+
+
+function show_details(path, pg::PulseGraph)
+    println("--------------Path Details ----------------------")
+    println(path)
+    j = path[1]
+    arr_time = 0.0
+    load = 0.0
+    cost = 0.0
+    cost_change = 0.0
+    println("At node $j: time=$(round(arr_time)), load=$(round(load)), cost=$(round(cost)), cost_change=$cost_change")
+    for k in 2:length(path)
+        i, j = path[k-1], path[k]
+        arr_time = max(arr_time + pg.time[i,j], pg.early_time[j])
+        load += pg.load[i,j]
+        cost += pg.cost[i,j]
+        cost_change = pg.cost[i,j]
+        println("At node $j: time=$(round(arr_time)), load=$(round(load)), cost=$(round(cost)), cost_change=$cost_change")    
+    end      
+    println("-"^50)  
+end
+
+
+# show_details(sol.path, pg)
+# show_details(lab.path, pg)
+
+# show_details([51, 20, 22, 31, 2, 6, 50, 32, 33, 37, 36, 7], pg)
+# show_details([51, 20, 22, 31, 2, 6, 50, 32, 34, 12, 38, 7], pg)
+
+
+# for i in 1:length(sol.path)
+#     println("--"^20)
+#     @show i, sol.path[i], sol.path[1:i]
+#     find_min_cost_label!(labelset[sol.path[i]])
+#     for label in labelset[36]
+#         if label.path == sol.path[1:i]
+#             @show label.cost, label.path, label.unreachable[sol.path[i+1]]
+#         end
+#     end
+#     for label in labelset[7]
+#         if label.path == sol.path[1:i]
+#             @show label.cost, label.path, label.unreachable[sol.path[i+1]]
+#         end
+#     end    
+# end
+
+
+
+# println("Route: ", sol.path)
+# println("Total Cost = ", sol.cost)
+# println("Total Load = ", sol.load)
+# println("Total Time = ", sol.time)
+# -129.43392203005823
+# @assert isapprox(-129.43392203005823, lab.cost, atol=1e-7)
+
+# @testset "ESPPRC-Example" begin
+#     @test isapprox(sol.cost, -1.124519917849354)
+# end
