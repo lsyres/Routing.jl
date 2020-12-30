3b3d3f3ff6fbf22f60068eac2eefdcafbf5408fc
Changhyun Kwon <chkwon@gmail.com>
2020-12-30 12:19:12 -0500

    testing reorganized

--- examples/espprc_example.jl
+++ examples/espprc_example.jl
@@ -108,66 +108,9 @@ solution_label = solveESPPRC(pg)
 #     path        ::Vector{Int}
 # end
 
+@show solution_label.path
+@show solution_label.cost
+@show solution_label.time
+@show solution_label.load
 
 
-@info("Testing for OD=($(pg.origin), $(pg.destination))")
-
-print("Pulse     : "); @time pulse = solveESPPRC(pg, method="pulse")
-print("Mono      : "); @time mono0= solveESPPRC(pg, method="monodirectional")
-print("Mono DSSR : "); @time mono1 = solveESPPRC(pg, method="monodirectional", DSSR=true)
-# print("Bi        : "); @time bidi0= solveESPPRC(pg, method="bidirectional")
-# print("Bi   DSSR : "); @time bidi1 = solveESPPRC(pg, method="bidirectional", DSSR=true)
-
-@show pulse.cost, pulse.load, pulse.time
-@show mono0.cost, mono0.load, mono0.time
-@show mono1.cost, mono1.load, mono1.time
-# @show bidi0.cost, bidi0.load, bidi0.time
-# @show bidi1.cost, bidi1.load, bidi1.time
-@show pulse.path
-@show mono0.path
-@show mono1.path
-# @show bidi0.path
-# @show bidi1.path
-
-@testset "ESPPRC Example Test" begin
-    @test isapprox(pulse.cost, mono0.cost, atol=1e-7)
-    @test isapprox(mono0.cost, mono1.cost, atol=1e-7)
-    # @test isapprox(pulse.cost, bidi0.cost, atol=1e-7)
-    # @test isapprox(bidi0.cost, bidi1.cost, atol=1e-7)
-end
-
-
-
-# negative cost route return test 
-max_neg = 10
-@info("ESPPRC Example testing with max_neg_routes=$(max_neg)...")
-
-@time pulse, neg_pulse = solveESPPRC_vrp(pg, method="pulse", max_neg_routes=max_neg)
-@time mono0, neg_mono0 = solveESPPRC_vrp(pg, method="monodirectional", max_neg_routes=max_neg)
-@time mono1, neg_mono1 = solveESPPRC_vrp(pg, method="monodirectional", max_neg_routes=max_neg, DSSR=true)
-
-@show typeof(pulse), typeof(neg_pulse), length(neg_pulse)
-@show pulse 
-@show mono0
-@show mono1
-
-@testset "ESPPRC Example Test with max_neg_routes" begin
-    @test length(neg_pulse) <= max_neg
-    @test length(neg_mono0) <= max_neg
-    @test length(neg_mono1) <= max_neg
-    for l in neg_pulse
-        @test l.cost < 0.0
-        @test l.path[1] == pg.origin        
-        @test l.path[end] == pg.destination
-    end
-    for l in neg_mono0
-        @test l.cost < 0.0
-        @test l.path[1] == pg.origin        
-        @test l.path[end] == pg.destination        
-    end
-    for l in neg_mono1
-        @test l.cost < 0.0
-        @test l.path[1] == pg.origin        
-        @test l.path[end] == pg.destination        
-    end
-end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,35 +6,33 @@ include("debugging.jl")
 test_start_time = time()
 
 
-include("../examples/espprc_example.jl")
+include("espprc_test.jl")
+include("vrptw_test.jl")
 
-@testset "VRPTW" begin
-    include("../examples/vrptw_example.jl")
 
-    @testset "Solomon Instance Test" begin
-        solomon_dataset = Dict(
-            "C101_025" => 191.30,
-            "C102_025" => 190.30,
-            "C201_025" => 214.70,
-            "RC105_025" => 411.30,
-            "R101_025" => 617.10,
-            "R102_025" => 547.10,
-        )
+@testset "Solomon Instance Test" begin
+    solomon_dataset = Dict(
+        "C101_025" => 191.30,
+        "C102_025" => 190.30,
+        "C201_025" => 214.70,
+        "RC105_025" => 411.30,
+        "R101_025" => 617.10,
+        "R102_025" => 547.10,
+    )
 
-        for (name, val) in solomon_dataset
-            @info("Solving the Solomon Instance $name")
-            solomon = load_solomon(name)
-            vrptw = generate_solomon_vrptw_instance(solomon)
+    for (name, val) in solomon_dataset
+        @info("Solving the Solomon Instance $name")
+        solomon = load_solomon(name)
+        vrptw = generate_solomon_vrptw_instance(solomon)
 
-            @testset "$name" begin
-                println("-- Solomon Instance $name --")
-                @time routes, objective_value = solveVRP(vrptw, pricing_method="pulse");
-                @test isapprox(objective_value, val, atol=1e-7)
-            end
+        @testset "$name" begin
+            println("-- Solomon Instance $name --")
+            @time routes, objective_value = solveVRP(vrptw, pricing_method="pulse");
+            @test isapprox(objective_value, val, atol=1e-7)
         end
     end
-
 end
+
 
 total_test_time = time() - test_start_time
 @show total_test_time
